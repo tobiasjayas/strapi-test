6924d8089c05c473cab28e0da4f94894c700ef6c
tobiasjayas <tobias@studioalva.co>
2020-11-13 15:57:50 +0700

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ddec3228e1cef222d371ef427cafc5b0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '36f4fb9e-0bbd-409a-b2b2-ffefaa3a1a40'
+};
